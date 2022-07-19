142e20e162610e3c5b7d82981bcfb7c9d86c3bee
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-19 14:09:48 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,57 @@
+﻿using la_mia_pizzeria_razor_layout.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace la_mia_pizzeria_razor_layout.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas = db.Pizza.ToList();
+                return View(pizzas);
+            }
+        }
+        public IActionResult Detail(int id)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaFound = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+                if(pizzaFound == null)
+                {
+                    return View("Error");
+                }
+                else
+                {
+                return View("Detail",pizzaFound);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreateForm", pizza);
+            }
+            using(PizzaContext context = new PizzaContext()) 
+            {
+                context.Add(new Pizza(pizza.Name, pizza.Description, pizza.Image, pizza.Price));
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        [HttpGet]
+        public IActionResult CreateForm()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_razor_layout.Models
+{
+    [Table("pizza")]
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        [Range(1,30)]
+        public float Price { get; set; }
+
+        public Pizza()
+        {
+
+        }
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
